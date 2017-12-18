c31427f8361d931da775e36e25a8973a18dd6030
kdebrab <karel.debrabandere@3e.eu>
2017-12-18 21:19:26 +0100

    single-source package version for setup and opengrid.__version__

---
+++ opengrid/__about__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+__all__ = ['__name__', '__summary__', '__url__', '__version__', '__author__', '__email__', '__license__']
+
+__name__ = 'opengrid'
+__summary__ = 'Open-source algorithms for data-driven building analysis and control'
+__version__ = '0.5.3'
+__author__ = 'Roel De Coninck and many others'
+__email__ = 'roeldeconinck@gmail.com'
+__license__ = 'Apache 2.0'
+__url__ = 'https://github.com/opengridcc/opengrid'
+__keywords__ = 'algorithms buildings monitoring analysis control'
