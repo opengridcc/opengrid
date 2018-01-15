bb5b0aea17c4a34cda641307c554650e6c50a5d8
Jan Pecinovsky <janpecinovsky@gmail.com>
2018-01-15 22:08:48 +0100

    version bump

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
     ],
 
     keywords=about['__keywords__'],
