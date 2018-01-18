42d170079f991afaa7a443142d60a6cb3a8face6
saroele <roeldeconinck@gmail.com>
2018-01-18 13:48:27 +0100

    * Adding 6 last days of 2015 to weather dataset of 2016 for degree-day computation * Modification of compute degree-days notebook to reflect the change

--- opengrid/__init__.py
+++ opengrid/__init__.py
@@ -1,6 +1,5 @@
 from opengrid.__about__ import __version__
 import opengrid.library.analysis as analysis
-import opengrid.library.exceptions as exceptions
 from opengrid.library.regression import MultiVarLinReg
 import opengrid.datasets as datasets
 from opengrid.library.plotting import plot_style
