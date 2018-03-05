7a91d2923d3ccbc95fdb929e58a94585b6c94654
saroele <roeldeconinck@gmail.com>
2018-03-05 14:09:19 +0100

    Version bump 0.5.7

--- opengrid/__about__.py
+++ opengrid/__about__.py
@@ -3,7 +3,7 @@ __all__ = ['__title__', '__summary__', '__url__', '__version__', '__author__', '
 
 __title__ = 'opengrid'
 __summary__ = 'Open-source algorithms for data-driven building analysis and control'
-__version__ = '0.5.6'
+__version__ = '0.5.7'
 __author__ = 'Roel De Coninck and many others'
 __email__ = 'roeldeconinck@gmail.com'
 __license__ = 'Apache 2.0'
