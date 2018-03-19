978558768d0fcdc47638c14a771aa11692aaa800
kdebrab <karel.debrabandere@3e.eu>
2018-03-19 20:32:55 +0100

    update url

--- opengrid/library/utils.py
+++ opengrid/library/utils.py
@@ -3,19 +3,20 @@
 General util functions
 
 """
+import datetime
 import pandas as pd
 
 
-def week_schedule(index, on_time='9:00', off_time='17:00', off_days=['Sunday', 'Monday']):
+def week_schedule(index, on_time='9:00', off_time='17:00', off_days=None):
     """ Return boolean time series following given week schedule.
 
     Parameters
     ----------
     index : pandas.DatetimeIndex
         Datetime index
-    on_time : str
+    on_time : str or datetime.time
         Daily opening time. Default: '09:00'
-    off_time : str
+    off_time : str or datetime.time
         Daily closing time. Default: '17:00'
     off_days : list of str
         List of weekdays. Default: ['Sunday', 'Monday']
@@ -32,7 +33,15 @@ def week_schedule(index, on_time='9:00', off_time='17:00', off_days=['Sunday', '
     >>> index = pd.date_range('20170701', '20170710', freq='H')
     >>> week_schedule(index)
     """
-    on_time = pd.to_datetime(on_time, format='%H:%M').time()
-    off_time = pd.to_datetime(off_time, format='%H:%M').time()
+    if on_time is None:
+        on_time = '9:00'
+    if off_time is None:
+        off_time = '17:00'
+    if off_days is None:
+        off_days = ['Sunday', 'Monday']
+    if not isinstance(on_time, datetime.time):
+        on_time = pd.to_datetime(on_time, format='%H:%M').time()
+    if not isinstance(off_time, datetime.time):
+        off_time = pd.to_datetime(off_time, format='%H:%M').time()
     times = (index.time >= on_time) & (index.time < off_time) & (~index.weekday_name.isin(off_days))
     return pd.Series(times, index=index)
