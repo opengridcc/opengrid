13ebb4f15abb954176206000707e79288a406b99
kdebrab <karel.debrabandere@3e.eu>
2018-04-09 22:11:50 +0200

    update url

--- opengrid/__init__.py
+++ opengrid/__init__.py
@@ -1,5 +1,5 @@
 from opengrid.__about__ import __version__
-import opengrid.library.analysis as analysis
+from opengrid.library import analysis, plotting
 from opengrid.library.regression import MultiVarLinReg
-import opengrid.datasets as datasets
+from opengrid import datasets
 from opengrid.library.plotting import plot_style
