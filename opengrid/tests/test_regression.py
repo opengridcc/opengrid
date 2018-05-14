996134e3478ff4a79467aefc76cbe6e994080cb3
saroele <roeldeconinck@gmail.com>
2018-05-14 21:58:57 +0200

    Tried to test the exception but the test fails

--- opengrid/tests/test_regression.py
+++ opengrid/tests/test_regression.py
@@ -29,6 +29,13 @@ class RegressionTest(unittest.TestCase):
         mvlr.do_analysis()
         self.assertTrue(hasattr(mvlr, 'list_of_fits'))
 
+    # def test_raises(self):
+    #     df = datasets.get('gas_2016_hour')
+    #     df_month = df.resample('MS').sum()
+    #     mvlr = og.MultiVarLinReg(df_month, '313b', p_max=0.04)
+    #     self.assertRaises(UnboundLocalError, mvlr.fit)
+    #     self.assertRaises(UnboundLocalError, mvlr.list_of_fits)
+
     def test_strange_names(self):
         df = datasets.get('gas_2016_hour')
         df_month = df.resample('MS').sum()
