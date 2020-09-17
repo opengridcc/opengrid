3b18d5a73865e63af0897b0b43c1232fed73a87c
Evert Borghgraef <evert.borghgraef@gmail.com>
2020-09-18 01:14:10 +0200

    coveralls

--- setup.py
+++ setup.py
@@ -42,8 +42,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6'
     ],
-    package_dir={"": "src"},
-    packages=find_packages("src"),
+    package_dir={},
+    packages=find_packages(),
     install_requires=[
         'matplotlib',
         'numpy',
