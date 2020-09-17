d0024d5356554a5bb006f143b9435cd32715ef65
Evert Borghgraef <evert.borghgraef@gmail.com>
2020-09-17 20:42:33 +0200

    reformat + bump version of about file

--- opengrid/__about__.py
+++ opengrid/__about__.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
-__all__ = ['__title__', '__summary__', '__url__', '__version__', '__author__', '__email__', '__license__']
+
+"""  Information on the Opengrid project """
+
+__all__ = ['__title__', '__summary__', '__url__',
+           '__version__', '__author__', '__email__', '__license__']
 
 __title__ = 'opengrid'
 __summary__ = 'Open-source algorithms for data-driven building analysis and control'
-__version__ = '0.5.10'
-__author__ = 'Roel De Coninck and many others'
-__email__ = 'roeldeconinck@gmail.com'
+__version__ = '0.6.0'
+__author__ = 'Opengrid inc'
+__email__ = 'support@opengrid.com'
 __license__ = 'Apache 2.0'
 __url__ = 'https://opengridcc.github.io'
 __keywords__ = 'algorithms buildings monitoring analysis control'
