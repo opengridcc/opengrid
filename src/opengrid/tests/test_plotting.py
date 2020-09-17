b25e51be529fb86ba121a7a9393a5fa9ee8eb991
Evert Borghgraef <evert.borghgraef@gmail.com>
2020-09-17 22:05:00 +0200

    Move import statements to the top

--- src/opengrid/tests/test_plotting.py
+++ src/opengrid/tests/test_plotting.py
@@ -6,6 +6,7 @@ Created on Mon Dec 30 02:37:25 2013
 """
 
 import unittest
+import numpy as np
 import pandas as pd
 from opengrid.library import plotting
 
@@ -17,30 +18,28 @@ class PlotStyleTest(unittest.TestCase):
 
 class CarpetTest(unittest.TestCase):
     def test_default(self):
-        import numpy as np
         index = pd.date_range('2015-1-1', '2015-2-1', freq='h')
-        ser = pd.Series(np.random.normal(size=len(index)), index=index, name='abc')
+        ser = pd.Series(np.random.normal(size=len(index)),
+                        index=index, name='abc')
         assert plotting.carpet(ser) is not None
 
     def test_empty(self):
         assert plotting.carpet(pd.Series(index=list('abc'))) is None
 
+
 class BoxplotTest(unittest.TestCase):
     def test_default(self):
-        import numpy as np
-        import pandas as pd
-        from opengrid.library import plotting
         index = pd.date_range('2015-1-1', '2015-2-1', freq='d')
-        df = pd.DataFrame(index=index, data=np.random.randint(5, size=(len(index),20)))
+        df = pd.DataFrame(index=index, data=np.random.randint(
+            5, size=(len(index), 20)))
         plotting.boxplot(df)
 
     def test_arguments(self):
-        import numpy as np
-        import pandas as pd
-        from opengrid.library import plotting
         index = pd.date_range('2015-1-1', '2015-2-1', freq='d')
-        df = pd.DataFrame(index=index, data=np.random.randint(5, size=(len(index),20)))
-        plotting.boxplot(df, plot_mean=True, plot_ids=[2, 3], title="Title", xlabel="xlable", ylabel="ylable")
+        df = pd.DataFrame(index=index, data=np.random.randint(
+            5, size=(len(index), 20)))
+        plotting.boxplot(df, plot_mean=True, plot_ids=[
+                         2, 3], title="Title", xlabel="xlable", ylabel="ylable")
 
 
 if __name__ == '__main__':
